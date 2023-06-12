8b11a2bd6732011d6728108a2793ad86b5fe8949
haji4ref <haji4ref@gmail.com>
2023-06-12 15:42:09 +0330

    adds generate docs script

---
+++ genereate-docs.sh
@@ -0,0 +1,1 @@
+npx docusaurus-prince-pdf -u http://localhost:3000 --output test.pdf --append /prerequisite,/installation,/configuration,/invisible%20captcha,/plugins,/API/Verify,/API/healthCheck,/Widget%20methods/setColor,/Widget%20methods/setLang,/Widget%20methods/setTheme
